af3cc3e72c33bf584bb01877782e7b705a8c453c
Aarshad Devani <addy.aarshad@gmail.com>
2024-09-22 12:27:04 -0500

    Updating Header & Footer

--- components/core/AppFooter.vue
+++ components/core/AppFooter.vue
@@ -1,81 +1,64 @@
 <template>
-  <v-container fluid class="px-5 mt-8" style="background-color: #202124">
+  <v-container class="px-5 mt-8">
     <v-row
       justify-center
       align="center"
       style="max-width: 1400px; margin-left: auto; margin-right: auto"
     >
-      <v-col md="2" cols="12">
-        <h2 style="color: white">{{ mainData.communityName }}</h2>
-        <a
-          href="https://github.com/oss-labs/"
-          style="text-decoration: none; color: white"
-          ><p style="font-size: 70%"><i>developed by OSS Labs</i></p></a
-        >
+      <v-col sm="12" md="4" cols="12">
+        <h2>{{ mainData.communityName }}</h2>
+
+        <p>Designed and developed by the Community</p>
       </v-col>
-      <v-col md="10" cols="12">
-        <v-container fluid class="pa-0">
-          <v-row justify-center align="center">
-            <v-col md="9" cols="12">
-              <a
-                href="https://developers.google.com/community/gdg"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="link"
-                class="grey--text text--darken-3 mr-6 google-font"
-                style="text-decoration: none; color: white"
-                >About GDG Program</a
-              >
-              <br class="hidden-md-and-up" />
-              <a
-                href="https://www.womentechmakers.com/"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="link"
-                class="grey--text text--darken-3 mr-6 google-font"
-                style="text-decoration: none; color: white"
-                >About WTM Program</a
-              >
+      <v-col sm="12" md="8" cols="12" style="display: flex;justify-content: flex-end;">
+        <a
+          href="https://developers.google.com/community/gdg"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="link"
+          class="grey--text text--darken-3 mr-6 google-font"
+          style="text-decoration: none; color: black;font-size: 70%"
+          >GDG Program</a
+        >
+        <br class="hidden-md-and-up" />
+        <a
+          href="https://www.womentechmakers.com/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="link"
+          class="grey--text text--darken-3 mr-6 google-font"
+          style="text-decoration: none; color: black;font-size: 70%"
+          >WTM Program</a
+        >
 
-              <br class="hidden-md-and-up" />
-              <router-link
-                to="/faq"
-                rel="noreferrer"
-                aria-label="link"
-                class="mr-6 google-font grey--text text--darken-3"
-                style="text-decoration: none; color: white"
-                >FAQ</router-link
-              >
-              <br class="hidden-md-and-up" />
-              <router-link
-                to="/coc"
-                style="text-decoration: none; color: white"
-                rel="noreferrer"
-                aria-label="link"
-                class="mr-6 google-font grey--text text--darken-3"
-                >Code of conduct</router-link
-              >
-              <br class="hidden-md-and-up" />
-              <a
-                href="https://developers.google.com/community-guidelines"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="link"
-                class="mr-6 google-font grey--text text--darken-3"
-                style="text-decoration: none; color: white"
-                >Community Guidelines</a
-              >
-            </v-col>
-
-            <v-col md="3" cols="12">
-              <CommonSpeakerSocialButton
-                :socialLinks="mainData.communityLinks"
-                :dark="true"
-                class="footer-icons"
-              />
-            </v-col>
-          </v-row>
-        </v-container>
+        <br class="hidden-md-and-up" />
+        <router-link
+          to="/faq"
+          rel="noreferrer"
+          aria-label="link"
+          class="mr-6 google-font grey--text text--darken-3"
+          style="text-decoration: none; color: black;font-size: 70%"
+          >FAQ</router-link
+        >
+        <br class="hidden-md-and-up" />
+        <router-link
+          to="/coc"
+          style="text-decoration: none; color: black;font-size: 70%"
+          rel="noreferrer"
+          aria-label="link"
+          class="mr-6 google-font grey--text text--darken-3"
+          >Code of conduct</router-link
+        >
+        <br class="hidden-md-and-up" />
+        <a
+          href="https://developers.google.com/community-guidelines"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="link"
+          class="mr-6 google-font grey--text text--darken-3"
+          style="text-decoration: none; color: black;font-size: 70%"
+          >Community Guidelines</a
+        >
       </v-col>
     </v-row>
   </v-container>
